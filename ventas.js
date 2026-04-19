31019156be8899f37206c228797fac53394499fa
Carlinux89 <carlosj_1912_17@hotmail.com>
2026-04-19 00:42:17 -0500

    feat: agrega validaciones a inputs con mensajes de error

--- ventas.js
+++ ventas.js
@@ -9,18 +9,42 @@ function calcularComision(numeroVentas, precioProducto) {
     return comision;
 }
 
+function validarNumero(input, idError) {
+    const valor = input.value.trim();
+    const error = document.getElementById(idError);
+
+    // limpiar error previo
+    error.textContent = "";
+
+    // No vacío
+    if (valor === "") {
+        error.textContent = "Este campo es obligatorio";
+        return;
+    }
+
+    // Solo números
+    if (!/^\d+$/.test(valor)) {
+        error.textContent = "Solo se permiten números";
+        return;
+    }
+
+    // Máximo 5 dígitos
+    if (valor.length > 5) {
+        error.textContent = "Máximo 5 dígitos permitidos";
+        return;
+    }
+}
+
 function calcular() {
-    //let salarioBase = parseFloat(document.getElementById("txtSueldoBase").value);
-    //let salarioBase = parseFloat(recuperarTexto("txtSueldoBase"));
+
     let salarioBase = recuperarFloat("txtSueldoBase");
-    //let numeroVentas = parseInt(document.getElementById("txtVentas").value);
-    //let numeroVentas = parseInt(recuperarTexto("txtVentas"));
     let numeroVentas = recuperarFloat("txtVentas");
-    //let precioProducto = parseFloat(document.getElementById("txtPrecio").value);
-    //let precioProducto = parseFloat(recuperarTexto("txtPrecio"));
     let precioProducto = recuperarFloat("txtPrecio");
+
     let comision = calcularComision(numeroVentas, precioProducto);
+
     let salarioTotal = salarioBase + comision;
+
     document.getElementById("spSueldoBase").textContent = salarioBase.toFixed(2);
     document.getElementById("spComision").textContent = comision.toFixed(2);
     document.getElementById("spTotal").textContent = salarioTotal.toFixed(2);
